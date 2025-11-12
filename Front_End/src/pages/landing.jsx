31e2dee18b06c7365c667a643e2821888ecc7c3f
Aakash Bairagi <crounten@email.com>
2025-11-12 13:01:44 +0530

    update for MyMeet

---
+++ Front_End/src/pages/landing.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import "../App.css";
+import { Link } from 'react-router-dom' 
+
+export default function LandingPage() {
+  return (
+    <div className='landingPageContainer'>
+          <nav>
+              <div className='naveHeader'>
+                   <h2>My<span style={{color: "#FF9839"}}>Meet</span></h2>
+              </div>
+              <div className='navlist'>
+                   <p>Join as Guest</p>
+                   <p>Register</p>
+                   <div role='button'>
+                       <p>Login</p>
+                   </div>
+              </div>
+          </nav>
+
+
+          <div className="landingMainContainer">
+                <div>
+                  <h1><span style={{color: "#FF9839"}}>Connect</span> with your loved Ones</h1>
+                   
+                  <p>Cover a distance by My<span style={{color: "#FF9839"}}>Meet</span></p>
+                  <div role='button'>
+                      <Link to={"/auth"}>Get Started</Link>
+                  </div>
+                </div>
+                <div>
+
+                   <img src="/mobile.png" alt="" />
+
+                </div>
+          </div>
+    </div>
+  )
+}
+
